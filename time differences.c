0fc9f304718ca82f42410d71cc2bc187262fdccf
Gopala krishnan H <gopalakrishnan.hari95@gmail.com>
2016-06-29 13:17:59 +0530

    Create time differences.c

---
+++ time differences.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int t1,m1,t2,m2,a,b,c,e,d;
+clrscr();
+printf("\n enter first time:");
+scanf("\n%d%d",&t1,&m1);
+a=t1*60;
+d=m1+a;
+printf("\n enter second time:");
+scanf("\n%d%d",&t2,&m2);
+b=t2*60;
+e=m2+b;
+c=e-d;
+printf("\ndifferent between two given time :%d"m,c);
+getch();
+}
